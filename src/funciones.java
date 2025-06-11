9593af4d695dc94bf6d2eed2f84caac26ecfc542
mateo <mateomaldonado25@gmail.com>
2025-06-10 22:50:17 -0300

    ejercicios 1-3

---
+++ src/funciones.java
@@ -0,0 +1,18 @@
+public class funciones {
+
+    // 1. Mostrar mensaje fijo
+    public void mostrarSaludo() {
+        System.out.println("Hola, mundo");
+    }
+
+    // 2. Saludo personalizado
+    public void saludar(String nombre) {
+        System.out.println("Hola, " + nombre);
+    }
+
+    // 3. Área de un rectángulo
+    public double calcularArea(double base, double altura) {
+        return base * altura;
+    }
+
+}
